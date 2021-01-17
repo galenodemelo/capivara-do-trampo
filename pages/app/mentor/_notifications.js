2dfd836d8d294518314259012c1e40525d63d10e
Galeno de Melo <galenodemelo@gmail.com>
2021-01-17 11:10:54 -0300

    feat: Adicionando aba com notificações

---
+++ pages/app/mentor/_notifications.js
@@ -0,0 +1,38 @@
+export default function Notifications() {
+    return (
+        <ul className="mdl-list mdl-list--stripped boxed-content">
+            <li className="mdl-list__item mdl-list__item--two-line">
+                <span className="mdl-list__item-primary-content">
+                    <span>Mariana foi contratada!</span>
+                    <span className="mdl-list__item-sub-title">
+                        Parabéns pelo excelente trabalho! Como recompensa, depositamos uma comissãozinha na sua conta. Avante, Capivaras!
+                    </span>
+                </span>
+            </li>
+            <li className="mdl-list__item mdl-list__item--two-line">
+                <span className="mdl-list__item-primary-content">
+                    <span>Jorge aceitou seu convite</span>
+                    <span className="mdl-list__item-sub-title">
+                        Agora ele faz parte dos seus mentorados.
+                    </span>
+                </span>
+            </li>
+            <li className="mdl-list__item mdl-list__item--two-line">
+                <span className="mdl-list__item-primary-content">
+                    <span>Cristina aceitou seu convite</span>
+                    <span className="mdl-list__item-sub-title">
+                        Agora ela faz parte dos seus mentorados.
+                    </span>
+                </span>
+            </li>
+            <li className="mdl-list__item mdl-list__item--two-line">
+                <span className="mdl-list__item-primary-content">
+                    <span>Bem-vinda, Capivara!</span>
+                    <span className="mdl-list__item-sub-title">
+                        Estamos muito felizes de ter você conosco! Vamos juntos ajudar os jovens do Brasil a terem um futuro brilhante.
+                    </span>
+                </span>
+            </li>
+        </ul>
+    )
+}
--- pages/app/mentor/index.js
+++ pages/app/mentor/index.js
@@ -12,10 +12,16 @@ export default function Mentor() {
             <div className="mdl-tabs mdl-js-tabs mdl-js-ripple-effect">
                 <div className="mdl-tabs__tab-bar">
                     <a href="/app/mentor" className="mdl-tabs__tab is-active">Candidaturas</a>
+                    <a href="#notifications" className="mdl-tabs__tab">
+                        <span className="mdl-badge" data-badge="4">Notificações</span>
+                    </a>
                 </div>
 
                 <div className="mdl-tabs__panel is-active" id="applications">
                     <Applications />
+                </div>
+                <div className="mdl-tabs__panel" id="notifications">
+                    <Notifications />
                 </div>
             </div>
         </Layout>
